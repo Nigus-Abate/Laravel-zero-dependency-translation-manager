9b4b6789212e924158568bbad407ef1e88fd2ef4
Nigus AB <59485106+Nigus-Abate@users.noreply.github.com>
2024-08-12 13:52:59 -0700

    panel.php

---
+++ config/panel.php
@@ -0,0 +1,25 @@
+<?php
+return array (
+  'date_format' => 'Y-m-d',
+  'time_format' => 'H:i:s',
+  'primary_language' => 'en',
+  'available_languages' => 
+  array (
+    'en' => 
+    array (
+      'name' => 'En',
+      'flag' => 'fi fi-us',
+  ),
+    'am' => 
+    array (
+      'name' => 'Am',
+      'flag' => 'fi fi-et',
+  ),
+    'fr' => 
+    array (
+      'name' => 'Fr',
+      'flag' => 'fi fi-fr',
+  ),
+),
+  'registration_default_role' => '2',
+);
