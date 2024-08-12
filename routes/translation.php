fcc4c8333ee9acd0d2e0ea31799de4403acdaeec
Nigus AB <59485106+Nigus-Abate@users.noreply.github.com>
2024-08-12 13:31:40 -0700

    web.php

--- routes/translation.php
+++ routes/translation.php
@@ -2,8 +2,6 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\Translation\TranslationController;
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', '2fa']], function () {
-
-// Route::prefix('super-admin')->group(function () {
 	Route::get('translations', [TranslationController::class, 'index'])->name('translations.index');
 	Route::get('translations/{lang}/edit', [TranslationController::class, 'edit'])->name('translations.edit');
 	Route::post('translations/{lang}/edit', [TranslationController::class, 'update'])->name('translations.update');
@@ -19,31 +17,6 @@ Route::post('translations/copy', function (Request $request) {
     ]);
     return redirect()->route('translations.index');
 })->name('translations.copy');
-
-	// Route to add a new key to the translations (from the modal)
+// Route to add a new key to the translations (from the modal)
 Route::post('translations/add-key/{lang}', [TranslationController::class, 'addKey'])->name('translations.addKey');
-// Route::post('translations/copy', [TranslationController::class, 'copy'])->name('translations.copy');
-
 });
-
-
-// // Route to list all languages
-// Route::get('translations', [TranslationController::class, 'index'])->name('translations.index');
-
-// // Route to edit translations for a specific language
-// Route::get('translations/edit/{lang}', [TranslationController::class, 'edit'])->name('translations.edit');
-
-// // Route to update translations for a specific language
-// Route::post('translations/update/{lang}', [TranslationController::class, 'update'])->name('translations.update');
-
-// // Route to edit a specific translation key
-// Route::get('translations/edit-key/{lang}/{file}/{key}', [TranslationController::class, 'editKey'])->name('translations.editKey');
-
-// // Route to update a specific translation key
-// Route::post('translations/update-key/{lang}/{file}/{key}', [TranslationController::class, 'updateKey'])->name('translations.updateKey');
-
-// // Route to add a new key to the translations (from the modal)
-// Route::post('translations/add-key/{lang}', [TranslationController::class, 'addKey'])->name('translations.addKey');
-
-// // Route to copy a language (add a new language)
-// Route::post('translations/copy', [TranslationController::class, 'copy'])->name('translations.copy');
